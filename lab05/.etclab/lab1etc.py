7de4a3cbfdf954f53014d51ddcb3a43fdf96208d
HelloArtty <chaithawat.sakl@kmutt.ac.th>
2022-10-01 02:17:58 +0700

    clearlab5

--- lab05/.etclab/lab1etc.py
+++
@@ -1,9 +0,0 @@
-def file_check():
-    try:
-        with open('myFile.txt', 'r') as file_read:
-            print(file_read.read())
-            print("Successfully print content in myFile.txt")
-    except FileNotFoundError:
-        print("Unable to open file myFile.txt ")
-        
-file_check()
